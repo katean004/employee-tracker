a7e9212e50594a421e135642013ad98fb459d869
kate <kan004@ucr.edu>
2020-07-31 20:50:41 -0700

    set up mysql, express, and inquirer

---
+++ server.js
@@ -0,0 +1,33 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var express = require("express");
+
+
+// Create express app instance.
+var app = express();
+
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "greatBay_DB"
+  });
+  

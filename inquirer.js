36eea71b0b8b3349a1306529be3e7e8ea5d2c2f6
kate <kan004@ucr.edu>
2020-07-31 22:39:01 -0700

    updated start function with more prompts and choices

--- inquirer.js
+++ inquirer.js
@@ -13,19 +13,36 @@ function start(){
         type: "list",
         message: "What would you like to do?",
         name: "startOptions",
-        choices:["Add Employee","Delete Employee","View Employees"]
+        choices:["Add Employee", "Delete Employee", "View All Employees", "View All Employees By Department", 
+        "View All Employees By Manager", "Update Employee Role", "Update Employee Manager"]
     }).then(function(startAns){
         if(startAns.startOptions === "Add Employee"){
             //run function that adds employee
-            console.log("Adding Employee");
+            console.log("Adding employee");
 
         }else if(startAns.startOptions === "Delete Employee"){
             //run function that deletes employee
-            console.log("Deleting Employee");
+            console.log("Deleting employee");
 
-        }else if(startAns.startOptions === "View Employees"){
+        }else if(startAns.startOptions === "View All Employees"){
             //run function that views employee
-            console.log("Viewing Employees");
+            console.log("Viewing all employees");
+
+        }else if(startAns.startOptions === "View All Employees By Department"){
+            //run function that views employee
+            console.log("Viewing all employees by department");
+
+        }else if(startAns.startOptions === "View All Employees By Manager"){
+            //run function that views employee
+            console.log("Viewing all employees by manager");
+
+        }else if(startAns.startOptions === "Update Employee Role"){
+            //run function that views employee
+            console.log("Updating employee role");
+
+        }else if(startAns.startOptions === "Update Employee Manager"){
+            //run function that views employee
+            console.log("Updating employee manager");
 
         }
     });
@@ -35,6 +52,8 @@ function start(){
 //Adding employees function
 function addEmp(){
     //ask users for employee info using inquirer and query connection add the data to the mysql tables 
+    
+
     //INSERT INTO department (name) VALUES ("Manufacturing");
 
     //call quit function to see if user wants to do other functions or quit
@@ -52,6 +71,8 @@ function deleteEmp(){
 //Viewing employees function 
 function viewEmp(){
     //query connection to mysql from department, roles, employee tables to view specific employee or all employees...
+
+    //console.table(employee info)
 
     //call quit function to see if user wants to do other functions or quit    
 }

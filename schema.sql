efc9cc0569c3c0ff84748552213e326c42c24932
kate <kan004@ucr.edu>
2020-08-01 18:06:29 -0700

    updated functions and moved everything into server js

--- schema.sql
+++ schema.sql
@@ -15,6 +15,7 @@ CREATE TABLE roles (
   title varchar(30) NOT NULL,
   salary DECIMAL(6, 2),
   department_id INT,
+  FOREIGN KEY(department_id) REFERENCES department(id) 
   PRIMARY KEY (id)
 );
 
@@ -24,5 +25,7 @@ CREATE TABLE employee (
   last_name varchar(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT,
+  FOREIGN KEY(role_id) REFERENCES roles(id) 
+  FOREIGN KEY(manager_id) REFERENCES employee(id) 
   PRIMARY KEY (id)
 );
--- server.js
+++ server.js
@@ -1,19 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var express = require("express");
-
-// Create express app instance.
-var app = express();
-
-
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -37,36 +23,324 @@ connection.connect(function(err) {
   }
 
   console.log("connected as id " + connection.threadId);
+  start()
 });
 
 
-// Get route
-app.get("/", function(req, res) {
-    connection.query("SELECT * FROM tasks;", function(err, data) {
-      if (err) throw err;
 
-      //return response 
-      //***DONT THINK WE NEED THIS BC WE NOT USING FRONT END
+//Ask user what to do
+function start(){
+
+    inquirer.prompt({
+        type: "list",
+        message: "What would you like to do?",
+        name: "startOptions",
+        choices:["Add Employee", "Delete Employee", "View All Employees", "View All Employees By Department", 
+        "View All Employees By Manager", "Update Employee Role", "Update Employee Manager","Add Roles"]
+    }).then(function(startAns){
+        if(startAns.startOptions === "Add Employee"){
+            //run function that adds employee
+            addEmp();
+
+        }else if(startAns.startOptions === "Delete Employee"){
+            //run function that deletes employee
+            deleteEmp();
+
+        }else if(startAns.startOptions === "View All Employees"){
+            //run function that views employee
+            viewAllEmp();
+
+        }else if(startAns.startOptions === "View All Employees By Department"){
+            //run function that views employee
+            viewDeptEmp();
+
+        }else if(startAns.startOptions === "View All Employees By Manager"){
+            //run function that views employee
+            viewManEmp();
+
+        }else if(startAns.startOptions === "Update Employee Role"){
+            //run function that views employee
+            updateRole();
+
+        }else if(startAns.startOptions === "Update Employee Manager"){
+            //run function that views employee
+            updateMan();
+
+        }else if(startAns.startOptions === "Add Roles"){
+            addRoles()
+        }
     });
-  });
 
-// Post route
-app.post("/", function(req, res) {
+}
 
-    connection.query("SELECT * FROM tasks;", function(err, data) {
-        if (err) throw err;
+//Adding employees function
+function addEmp(){
+
+  connection.query("SELECT * FROM roles",function(err,res){
+    if (err) throw err
   
-        //return response
-        //***DONT THINK WE NEED THIS BC WE NOT USING FRONT END
+    let myRoles = res.map(function(role){
+      return({
+              name:role.title,
+              value: role.id
+             })
+             //when user chooses dept name inquirer will choose the department id for them
+    })
+    connection.query("SELECT * FROM employee",function(err,res){
+      if (err) throw err
+    
+      let myEmployees = res.map(function(emp){
+        return({
+                name:`${emp.first_name} ${emp.last_name}`,
+                value: emp.id
+               })
+               //when user chooses dept name inquirer will choose the department id for them
+      })
+
+  //ask users for employee info using inquirer and query connection add the data to the mysql tables 
+  const questions = 
+  [{
+      type: "input",
+      message: "What is the employee's first name?",
+      name: "firstName"
+  },
+  {
+      type: "input",
+      message: "What is the employee's last name?",
+      name: "lastName"
+  },
+  {
+      type: "list",
+      message: "What is the employee's role id?",
+      name: "role",
+      choices:myRoles
+      //Should i have choices be roles form sql roles table? 
+      //or have hard coded choices here then add into sql roles table
+  },
+  {
+      type: "list",
+      message: "What is the employee's manager id?",
+      name: "manId",
+      choices:myEmployees
+  }];
+
+inquirer.prompt(questions).then(function(empInfo){
+//add empInfo to the table in sql
+
+var queryEmp = connection.query(
+  "INSERT INTO employee SET ?", 
+  //do i need the question mark here?
+  {
+    first_name: empInfo.firstName,
+    last_name: empInfo.lastName,
+    role_id: empInfo.role,
+    manager_id: empInfo.manId
+  },
+  function(err, res) {
+    if (err) throw err;
+    console.log(res.affectedRows + " product inserted!\n");
+    
+    //call quit function to see if user wants to do other functions or quit
+    quit();
+  }
+);
+
+console.log(queryEmp.sql);
+//will this show the new employee added?
+
       });
-  });
+    })
+  })   
+}
 
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
-  
-  
-  
+//Deleting employees function (Bonus)
+function deleteEmp(){
+    console.log("Deleting employee");
+
+    //query connection to mysql and show whole employee table of employee choices
+    //DELETE FROM table_names WHERE refer to employee id or id...
+
+    //call quit function to see if user wants to do other functions or quit
+}
+
+
+//Viewing all employees function 
+function viewAllEmp(){
+    console.log("Viewing all employees");
+
+    //query connection to mysql from department, roles, employee tables to view specific employee or all employees...
+    //console.table(employee info)
+    //view by id, first_name, last_name, title, department, salary, manager
+
+    //call quit function to see if user wants to do other functions or quit    
+}
+
+//Viewing employees by department function 
+function viewDeptEmp(){
+    console.log("Viewing all employees by department");
+
+    //query connection to mysql from department, roles, employee tables to view specific employee or all employees...
+    //console.table(employee info)
+    //view by id, first_name, last_name, title, department, salary, manager
+
+    //call quit function to see if user wants to do other functions or quit    
+}
+
+//Viewing employees by manager function 
+function viewManEmp(){
+    console.log("Viewing all employees by manager");
+
+    //query connection to mysql from department, roles, employee tables to view specific employee or all employees...
+    //console.table(employee info)
+    //view by id, first_name, last_name, title, department, salary, manager
+
+    //call quit function to see if user wants to do other functions or quit    
+}
+
+function viewDept
+
+//Update employee role function
+function updateRole(){
+    console.log("Updating employee role");
+    connection.query("SELECT * FROM roles",function(err,res){
+      if (err) throw err
+    
+      let myRoles = res.map(function(role){
+        return({
+                name:role.title,
+                value: role.id
+               })
+               //when user chooses dept name inquirer will choose the department id for them
+      })
+      connection.query("SELECT * FROM employee",function(err,res){
+        if (err) throw err
+      
+        let myEmployees = res.map(function(emp){
+          return({
+                  name:`${emp.first_name} ${emp.last_name}`,
+                  value: emp.id
+                 })
+                 //when user chooses dept name inquirer will choose the department id for them
+        })
+        
+      
+      inquirer.prompt([
+        {
+          type: "list",
+          message: "which Employee you like to change the role on??",
+          name: "emp",
+          choices:myEmployees
+          //Should i have choices be roles form sql roles table? 
+          //or have hard coded choices here then add into sql roles table
+      },
+      {
+          type: "list",
+          message: "What is the employee'snew role?",
+          name: "newRole",
+          choices:myRoles
+      
+      }]).then(function(ans){
+      //add empInfo to the table in sql
+      
+       connection.query(
+        "UPDATE  employee SET ? WHERE ?", 
+        //do i need the question mark here?
+        [
+          {
+            role_id : ans.newRole
+          },
+          {
+            id: ans.emp
+          }
+      ],
+        function(err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + " product inserted!\n");
+          
+          // what is affectedRows?
+          quit();
+        }
+      )
+      })
+
+      })
+      
+    })
+
+    //call quit function to see if user wants to do other functions or quit    
+}
+
+//Update employee manager function
+function updateMan(){
+    console.log("Updating employee manager");
+
+    //choose employee to update 
+    //query connection UPDATE table_name SET row_data = "new data" WHERE id = 2
+
+    //call quit function to see if user wants to do other functions or quit
+}
+
+//Add roles function
+function addRoles(){
+connection.query("SELECT * FROM department",function(err,res){
+  if (err) throw err
+
+  let myDepartments = res.map(function(dep){
+    return({
+            name:dep.name,
+            value: dep.id
+           })
+           //when user chooses dept name inquirer will choose the department id for them
+  })
+
+  inquirer.prompt([
+    {
+      type:"input",
+      name:"roleName",
+      message:"WHat is the name of the role?"
+    },
+    {
+      type:"input",
+      name:"salary",
+      message:"what is the salary for this role?"
+    },
+    {
+      type:"list",
+      name:"depId",
+      message:"What department this role belongs to?",
+      choices: myDepartments
+    }
+  ]).then(roleRes => {
+    console.log(roleRes);
+    connection.query("INSERT INTO roles SET ?",
+    {
+      title: roleRes.roleName,
+      salary: roleRes.salary,
+      department_id: roleRes.depId
+    })
+  }).then(roleErr =>{
+    if (roleErr) throw roleErr
+    start();
+  })
+});
+}
+
+//Quit function 
+function quit(){
+    //repeat start function when user is done with one action and see if they want to quit or continue other functions
+    inquirer.prompt({
+        type: "list",
+        message: "Are you finished?",
+        name: "endOptions",
+        choices:["Yes", "No"]
+    }).then(function(quitting){
+        if(quitting.endOptions === "No"){
+            start();
+        }else if(quitting.endOptions === "Yes"){
+            //console.table(all employee info)
+            console.log("All Done!");
+            connection.end()
+        }
+    });
+}

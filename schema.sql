6f17205b6d5b3e75eae67bd70c39d2b3673d44fd
kate <kan004@ucr.edu>
2020-08-02 20:03:12 -0700

    updated schema sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,11 @@
+-- Setup database
 DROP DATABASE IF EXISTS emptrack_db;
 
 CREATE DATABASE emptrack_db;
 
 USE emptrack_db;
 
+-- Setup tables
 CREATE TABLE department (
   dept_id int NOT NULL AUTO_INCREMENT,
   name varchar(30) NOT NULL,
@@ -29,3 +31,10 @@ CREATE TABLE employee (
   FOREIGN KEY(manager_id) REFERENCES employee(emp_id), 
   PRIMARY KEY (emp_id)
 );
+
+-- Joining tables schema
+SELECT name, title, salary, emp_id, first_name, last_name, manager_id
+FROM roles LEFT JOIN department
+ON roles.department_id = department.dept_id
+RIGHT JOIN employee 
+ON employee.role_id = roles.roles_id;
